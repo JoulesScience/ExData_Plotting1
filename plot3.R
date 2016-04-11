f72cdd10aaf6705c28f610c4add970b032f43dc6
JoulesScience <juliocesarpalacioojeda@gmail.com>
2016-04-10 22:53:18 -0500

    Create plot3.R

---
+++ plot3.R
@@ -0,0 +1,27 @@
+# Read Data
+data <- read.csv("household_power_consumption.txt", na.string="?", sep=";")
+
+# Extract needed dataset
+data <- data[(data$Date=="1/2/2007" | data$Date=="2/2/2007"),]
+
+# Combine Date and Time
+data$DateTime <- strptime(paste(data$Date, data$Time, sep=" "), 
+                          format="%d/%m/%Y %H:%M:%S")
+
+# Open png device
+png("plot3.png", width=480, height=480)
+
+# Plot the graph
+plot(data$DateTime, data$Sub_metering_1, type="l", 
+     xlab="", ylab="Energy sub metering")
+lines(data$DateTime, data$Sub_metering_2, col="red")
+lines(data$DateTime, data$Sub_metering_3, col="blue")
+legend("topright", col=c("black", "red", "blue"), 
+       legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+       lty=1)
+
+# Turn off device
+dev.off()
+
+
+##Reference:  https://github.com/sgloutnikov/jh-datascience/blob/master/4-exploratory_data_analysis/pa1/plot3.R
